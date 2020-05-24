bbf1dcd657a80ce11807d4839ac927ae96178080
kudas g <kudosdesigner@hotmail.com>
2020-05-23 22:35:45 -0400

    Updates

---
+++ precache-manifest.cace1ce38bd4acf195eb42f1e39a0c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b55895a0de28ea652ad47f5fd7500d",
+    "url": "/new_site/index.html"
+  },
+  {
+    "revision": "01294bd1681b0846822d",
+    "url": "/new_site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dbdb60cbc269ab92dfac",
+    "url": "/new_site/static/js/2.b8864cf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new_site/static/js/2.b8864cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01294bd1681b0846822d",
+    "url": "/new_site/static/js/main.f422072c.chunk.js"
+  },
+  {
+    "revision": "c9508bfedbe1ab60fd93",
+    "url": "/new_site/static/js/runtime-main.13e9edc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
